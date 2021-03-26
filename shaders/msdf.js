738cf5b43e6e5de8eda67c55777c1a85df3b0c5d
Jamie Cope <jamie@gcope.com>
2021-03-26 08:17:42 -0400

    Update msdf shader for WebGL2

--- shaders/msdf.js
+++ shaders/msdf.js
@@ -8,6 +8,7 @@ module.exports = function createMSDFShader (opt) {
   var color = opt.color;
   var map = opt.map;
   var negate = typeof opt.negate === 'boolean' ? opt.negate : true;
+  var webgl2 = opt.isWebGL2 || false;
 
   // remove to satisfy r73
   delete opt.map;
@@ -15,47 +16,98 @@ module.exports = function createMSDFShader (opt) {
   delete opt.precision;
   delete opt.opacity;
   delete opt.negate;
+  delete opt.isWebGL2;
 
-  return assign({
-    uniforms: {
-      opacity: { type: 'f', value: opacity },
-      map: { type: 't', value: map || new THREE.Texture() },
-      color: { type: 'c', value: new THREE.Color(color) }
-    },
-    vertexShader: [
-      'attribute vec2 uv;',
-      'attribute vec4 position;',
-      'uniform mat4 projectionMatrix;',
-      'uniform mat4 modelViewMatrix;',
-      'varying vec2 vUv;',
-      'void main() {',
-      'vUv = uv;',
-      'gl_Position = projectionMatrix * modelViewMatrix * position;',
-      '}'
-    ].join('\n'),
-    fragmentShader: [
-      '#ifdef GL_OES_standard_derivatives',
-      '#extension GL_OES_standard_derivatives : enable',
-      '#endif',
-      'precision ' + precision + ' float;',
-      'uniform float opacity;',
-      'uniform vec3 color;',
-      'uniform sampler2D map;',
-      'varying vec2 vUv;',
+  if(webgl2) {
+    return assign({
+      uniforms: {
+        opacity: { type: 'f', value: opacity },
+        map: { type: 't', value: map || new THREE.Texture() },
+        color: { type: 'c', value: new THREE.Color(color) }
+      },
+      vertexShader: [
+        '#version 300 es',
 
-      'float median(float r, float g, float b) {',
-      '  return max(min(r, g), min(max(r, g), b));',
-      '}',
+        'in vec2 uv;',
+        'in vec4 position;',
+        'uniform mat4 projectionMatrix;',
+        'uniform mat4 modelViewMatrix;',
+        'out vec2 vUv;',
 
-      'void main() {',
-      '  vec3 sample = ' + (negate ? '1.0 - ' : '') + 'texture2D(map, vUv).rgb;',
-      '  float sigDist = median(sample.r, sample.g, sample.b) - 0.5;',
-      '  float alpha = clamp(sigDist/fwidth(sigDist) + 0.5, 0.0, 1.0);',
-      '  gl_FragColor = vec4(color.xyz, alpha * opacity);',
-      alphaTest === 0
-        ? ''
-        : '  if (gl_FragColor.a < ' + alphaTest + ') discard;',
-      '}'
-    ].join('\n')
-  }, opt);
+        'void main() {',
+        '  vUv = uv;',
+        '  gl_Position = projectionMatrix * modelViewMatrix * position;',
+        '}'
+      ].join('\n'),
+      fragmentShader: [
+        '#version 300 es',
+
+        'precision ' + precision + ' float;',
+        'uniform float opacity;',
+        'uniform vec3 color;',
+        'uniform sampler2D map;',
+        'in vec2 vUv;',
+        'out vec4 myOutputColor;',
+
+        'float median(float r, float g, float b) {',
+        '  return max(min(r, g), min(max(r, g), b));',
+        '}',
+
+        'void main() {',
+        '  vec3 s = ' + (negate ? '1.0 - ' : '') + 'texture(map, vUv).rgb;',
+        '  float sigDist = median(s.r, s.g, s.b) - 0.5;',
+        '  float alpha = clamp(sigDist/fwidth(sigDist) + 0.5, 0.0, 1.0);',
+        '  myOutputColor = vec4(color.xyz, alpha * opacity);',
+        alphaTest === 0
+          ? ''
+          : '  if (myOutputColor.a < ' + alphaTest + ') discard;',
+        '}'
+      ].join('\n')
+    }, opt);
+  }
+  else
+  {
+    return assign({
+      uniforms: {
+        opacity: { type: 'f', value: opacity },
+        map: { type: 't', value: map || new THREE.Texture() },
+        color: { type: 'c', value: new THREE.Color(color) }
+      },
+      vertexShader: [
+        'attribute vec2 uv;',
+        'attribute vec4 position;',
+        'uniform mat4 projectionMatrix;',
+        'uniform mat4 modelViewMatrix;',
+        'varying vec2 vUv;',
+        'void main() {',
+        'vUv = uv;',
+        'gl_Position = projectionMatrix * modelViewMatrix * position;',
+        '}'
+      ].join('\n'),
+      fragmentShader: [
+        '#ifdef GL_OES_standard_derivatives',
+        '#extension GL_OES_standard_derivatives : enable',
+        '#endif',
+        'precision ' + precision + ' float;',
+        'uniform float opacity;',
+        'uniform vec3 color;',
+        'uniform sampler2D map;',
+        'varying vec2 vUv;',
+
+        'float median(float r, float g, float b) {',
+        '  return max(min(r, g), min(max(r, g), b));',
+        '}',
+
+        'void main() {',
+        '  vec3 sample = ' + (negate ? '1.0 - ' : '') + 'texture2D(map, vUv).rgb;',
+        '  float sigDist = median(sample.r, sample.g, sample.b) - 0.5;',
+        '  float alpha = clamp(sigDist/fwidth(sigDist) + 0.5, 0.0, 1.0);',
+        '  gl_FragColor = vec4(color.xyz, alpha * opacity);',
+        alphaTest === 0
+          ? ''
+          : '  if (gl_FragColor.a < ' + alphaTest + ') discard;',
+        '}'
+      ].join('\n')
+    }, opt);
+  }
 };
